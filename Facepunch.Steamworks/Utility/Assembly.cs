0739e1acfbd50fd165abefaf1a2802925e4b564e
Theodor Müller <info@trilliarden.net>
2024-07-20 19:35:58 +0200

    Add access to the executing System.Reflection.Assembly

---
+++ Facepunch.Steamworks/Utility/Assembly.cs
@@ -0,0 +1,7 @@
+namespace Steamworks {
+    public static class Assembly {
+        public static System.Reflection.Assembly Get() {
+            return System.Reflection.Assembly.GetExecutingAssembly();
+        }
+    }
+}
