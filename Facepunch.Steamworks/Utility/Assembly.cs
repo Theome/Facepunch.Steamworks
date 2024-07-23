42f56f16ae6863490bc9928e2bcf611181622bbc
Theodor Müller <info@trilliarden.net>
2024-07-23 11:18:48 +0200

    Remove unused Assembly class

--- Facepunch.Steamworks/Utility/Assembly.cs
+++
@@ -1,7 +0,0 @@
-namespace Steamworks {
-    public static class Assembly {
-        public static System.Reflection.Assembly Get() {
-            return System.Reflection.Assembly.GetExecutingAssembly();
-        }
-    }
-}
